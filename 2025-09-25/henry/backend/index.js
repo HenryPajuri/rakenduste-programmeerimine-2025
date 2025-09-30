f3a9973bc4735352db5059c2cf44224f1da6a542
Henry-Pajuri <Henry.Pajuri@gmail.com>
2025-09-30 18:52:14 +0300

    feat: Add controllers, middlewares and routes

--- 2025-09-25/henry/backend/index.js
+++ 2025-09-25/henry/backend/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const catsRoutes = require("./routes/cats.routes");
 
+app.use(express.json());
+
+app.use("/cats", catsRoutes);
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
