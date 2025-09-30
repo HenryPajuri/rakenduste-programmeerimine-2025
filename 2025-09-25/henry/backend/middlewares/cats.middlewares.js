ad58857d93c5409494c8b27e78f518acb4cfaeb9
Henry-Pajuri <Henry.Pajuri@gmail.com>
2025-09-30 20:25:00 +0300

    feat: Add express-validator middleware for cats
    Create validation middleware using express-validator for cat name and UUID validation for cat IDs.

--- 2025-09-25/henry/backend/middlewares/cats.middlewares.js
+++ 2025-09-25/henry/backend/middlewares/cats.middlewares.js
@@ -1,11 +1,45 @@
+const { body, param } = require("express-validator");
+
 const catsRouteMiddleware = (req, res, next) => {
-    console.log("Time: ", Date.now());
-    next();
-  };
-  
-  const catsGetRouteMiddleware = (req, res, next) => {
-    console.log("GET middleware");
-    next();
-  };
-  
-  module.exports = { catsRouteMiddleware, catsGetRouteMiddleware };
+  console.log("Time: ", Date.now());
+  next();
+};
+
+const catsGetRouteMiddleware = (req, res, next) => {
+  console.log("GET middleware");
+  next();
+};
+
+const validateCreateCat = [
+  body("name")
+    .isString()
+    .withMessage("Name must be a string")
+    .trim()
+    .notEmpty()
+    .withMessage("Name is required")
+    .isLength({ min: 2, max: 50 })
+    .withMessage("Name must be between 2 and 50 characters"),
+];
+
+const validateUpdateCat = [
+  body("name")
+    .isString()
+    .withMessage("Name must be a string")
+    .trim()
+    .notEmpty()
+    .withMessage("Name is required")
+    .isLength({ min: 2, max: 50 })
+    .withMessage("Name must be between 2 and 50 characters"),
+];
+
+const validateCatId = [
+  param("id").isUUID().withMessage("Invalid cat ID format"),
+];
+
+module.exports = {
+  catsRouteMiddleware,
+  catsGetRouteMiddleware,
+  validateCreateCat,
+  validateUpdateCat,
+  validateCatId,
+};
