62445893c9b92a8217d1ccd2439204e66c1b38ba
Henry-Pajuri <Henry.Pajuri@gmail.com>
2025-09-30 21:57:44 +0300

    feat: Integrate admin routes and add endpoint documentation
    Add admin routes to Express app and update root endpoint

--- 2025-09-25/TODO/backend/index.js
+++ 2025-09-25/TODO/backend/index.js
@@ -3,13 +3,22 @@ const cors = require('cors')
 const app = express()
 const port = 3001
 const todosRoutes = require("./routes/todos.routes");
+const adminRoutes = require("./routes/admin.routes");
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/todos", todosRoutes);
+app.use("/admin", adminRoutes);
 app.get('/', (req, res) => {
-  res.send('TODO API')
+  res.send(`
+    <h1>TODO API</h1>
+    <h2>Available Endpoints:</h2>
+    <ul>
+      <li><a href="/todos">/todos</a> - TODO CRUD operations</li>
+      <li><a href="/admin/todos">/admin/todos</a> - Admin panel (view all todos including deleted)</li>
+    </ul>
+  `)
 })
 
 app.listen(port, () => {
