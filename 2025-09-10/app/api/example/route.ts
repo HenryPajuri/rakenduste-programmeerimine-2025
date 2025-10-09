525712db8130818dc0dacc354a33807898c5ff17
Henry-Pajuri <Henry.Pajuri@gmail.com>
2025-10-09 13:41:28 +0300

    feat: add delete endpoint

--- 2025-09-10/app/api/example/route.ts
+++ 2025-09-10/app/api/example/route.ts
@@ -7,3 +7,7 @@ export async function GET(request: NextRequest) {
 export async function POST(request: NextRequest) {
   return NextResponse.json({ message: 'POST request successful' });
 }
+
+export async function DELETE(request: NextRequest) {
+  return NextResponse.json({ message: 'DELETE request successful' });
+}
