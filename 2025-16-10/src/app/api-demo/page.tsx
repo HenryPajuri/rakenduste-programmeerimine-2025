54854fa1b623a8381d4b0a3d86442b75368523fd
Henry-Pajuri <Henry.Pajuri@gmail.com>
2025-10-16 14:30:52 +0300

    feat: add a new api demo app to the project

---
+++ 2025-16-10/src/app/api-demo/page.tsx
@@ -0,0 +1,8 @@
+export default function ApiDemoPage() {
+  return (
+    <div>
+      <h1>API Demo</h1>
+      <p>This page will be used to demonstrate API fetching.</p>
+    </div>
+  );
+}
--- 2025-16-10/src/app/page.tsx
+++ 2025-16-10/src/app/page.tsx
@@ -30,6 +30,9 @@ export default function Home() {
           <Link href="/form">
             <button className="bg-gray-300 px-4 py-2 cursor-pointer">Go to User Form</button>
           </Link>
+          <Link href="/api-demo">
+            <button className="bg-gray-300 px-4 py-2 cursor-pointer">Go to API Demo</button>
+          </Link>
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
